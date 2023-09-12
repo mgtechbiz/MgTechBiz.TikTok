be4f13b59e0d70aece4c7d05389207296cfaa7c8
Mohamed Gharib <mohamed.gharib@mgtechbiz.com>
2023-09-12 16:53:10 -0400

    added web client and configured OIDC authentication

---
+++ MgTechBiz.TikTok.ContentPosting.WebApp/Program.cs
@@ -0,0 +1,58 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+    })
+    .AddCookie()
+    .AddOpenIdConnect(options =>
+    {
+        options.SignInScheme = "Cookies";
+        options.Authority = builder.Configuration["Oidc:Authority"];
+        options.RequireHttpsMetadata = true;
+        options.ClientId = builder.Configuration["Oidc:ClientId"];
+        options.ClientSecret = builder.Configuration["Oidc:ClientSecret"];
+        options.ResponseType = "code";
+        options.UsePkce = true;
+        //options.Scope.Add("profile");
+        options.SaveTokens = true;
+
+        options.Events = new OpenIdConnectEvents
+        {
+            OnTokenResponseReceived = context =>
+            {
+                //save the tokens
+
+                return Task.CompletedTask;
+            }
+        };
+    });
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
